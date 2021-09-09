be19e7ad0e14847f40dd855dc508d23b56ebd2cd
Shiloh Madsen <shiloh.madsen@gmail.com>
2021-09-09 12:34:03 -0700

    comitting node version of code. Revert to python

--- IC-Columns/IC-Column.js
+++ IC-Columns/IC-Column.js
@@ -1,18 +1,28 @@
 const Sentry = require("@sentry/node");
-const readline = require('readline');
+const readline = require("readline");
+const GPIO = require("onoff").Gpio;
 
 Sentry.init({
   dsn: "https://53058513222b41498b342be101261452@o358570.ingest.sentry.io/3153173",
   tracesSampleRate: 1.0,
 });
 
-const rl = readline.createInterface({
-  input: process.stdin,  
-  output: process.stdout
+const redLED = new Gpio(16, "out");
+const redButton = new Gpio(13, "in", "both");
+
+redButton.watch(function (err, value) {
+  if (err) {
+    console.error("There was an error", err);
+    return;
+  }
+  redLED.writeSync(value);
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  rl.close();
-});
+function unexportOnClose() {
+  redLED.writeSync(0);
+  redLED.unexport();
+  redButton.unexport();
+}
 
+// CLean Up on Ctrl+C
+process.on("SIGINT", unexportOnClose);
